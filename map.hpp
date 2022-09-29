8366a6b1d12df7a169284c1a5bfd51aaa665d3b8
fluorz <contact.fluorz@gmail.com>
2022-09-29 14:02:16 +0200

    Map in progress, defintion of member type

--- map.hpp
+++ map.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
-#include <utility>
-
+#include "pair.hpp"
+#include <memory>
+#include <iostream>
+#include "utils.hpp"
 
 namespace ft {
 
-    template < class Key, class T, class Alloc = allocator<pair<const Key,T> >
+    template < class Key, class T, class Compare = std::less<Key>, class Alloc = allocator<ft::pair<const Key,T> >
     class map {
         public:
-            typedef pair<const Key, T> value_type;
+            typedef Key                     Key_type;
+            typedef ft::pair<const Key, T>  value_type;
+            typedef T                       mapped_type;
+            typedef size_t                  size_type;
+            typedef Alloc                   allocator_type;
+            typedef Compare                 Key_compare;
         private:
 
         public:
